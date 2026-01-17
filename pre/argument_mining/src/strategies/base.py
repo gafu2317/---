56e92544b1c3da66cdc8d8973bb8fd9dca705383
gafu <0002317rynkmk@gmail.com>
2026-01-18 04:08:29 +0900

    ディレクトリ整理

--- pre/argument_mining/src/strategies/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from src.models import ArgumentGraph
-
-class MiningStrategy(ABC):
-    @abstractmethod
-    def analyze(self, text: str) -> ArgumentGraph:
-        pass
